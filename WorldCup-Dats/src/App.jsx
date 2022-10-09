c9b81fda6b0d2c1e034426fa29abdb191646f3d1
Alef Sloan <alef_sloan@hotmail.com>
2022-10-08 21:19:37 -0300

    feat: login page on App function

--- WorldCup-Dats/src/App.jsx
+++ WorldCup-Dats/src/App.jsx
@@ -1,11 +1,25 @@
-const Title = () => (
-  <h1>Título da Page</h1>
-)
-
 export function App() {
   return (
-    <div className="bg-red-500">
-      <Title />
+    <div className='h-screen bg-red-700 p-4 text-white flex flex-col items-center space-y-6'>
+      <header className='container max-w-5xl p-4 flex justify-center'>
+        <img className='w-40' src='/images/logo-wine.svg' />
+      </header>
+      <main className="container max-w-5xl p-6 flex-1 p-4 flex items-center flex-col md:flex-row space-y-6 md:space-y-0 md:space-x-6">
+        <div className='md:flex-1 flex justify-center'>
+          <img className='w-full' src='/images/login-img.png'/>
+        </div>
+        <div className='md:flex-1 flex flex-col space-y-6'>
+          <h1 className='text-3xl text-center md:text-left font-bold'>
+            Dê o seu palpite na Copa do Mundo do Catar 2022!
+          </h1>
+          <button className='bg-white text-red-700 text-xl px-8 py-5 rounded-xl'>
+            Criar minha conta
+          </button>
+          <button className='text-white border border-white text-xl px-8 py-5 rounded-xl'>
+            Login
+          </button>
+        </div>
+      </main>
     </div>
   )
 }
