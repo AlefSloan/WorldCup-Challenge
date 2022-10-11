30320c766ad8572e049cdbd0a5531d64d24b5990
Alef Sloan <alef_sloan@hotmail.com>
2022-10-10 21:00:55 -0300

    add Dashboard page

---
+++ WorldCup-Dats/src/pages/Dashboard/index.jsx
@@ -0,0 +1,46 @@
+import { Icon, Card } from '~/components'
+
+export const Dashboard = () => {
+  return (
+    <>
+      <header className='bg-red-300 text-white p-4'>
+        <div className='container max-w-3xl flex justify-between items-center p-4'>
+          <img src='./images/logo-red.svg' className='w-32'/>
+          <a href='/profile'>
+            <Icon name='profile' className='w-10' />
+          </a>
+        </div>
+      </header>
+      <main>
+        <section className='bg-red-300 text-white p-4'>
+          <div className='container max-w-3xl space-y-6 p-4'>
+            <span className='text-white'>Olá Alef!</span>
+            <h2 className='text-white text-2xl font-bold'>Qual é o seu palpite?</h2>
+          </div>
+        </section>
+        <div className='flex p-6 justify-center space-x-4'>
+          <Icon name='leftArrow' className='h-8'/>
+          <h4 className='text-xl font-bold text-red-700'>20 de novembro</h4>
+          <Icon name='rightArrow' className='h-8'/>
+        </div>
+        <section id='content' className='container max-w-3xl p-4 space-y-6'>
+          <Card
+            timeA={{ slug: 'sui'}}
+            timeB={{ slug: 'cam'}}
+            match={{ time: '7:00' }}
+          />
+          <Card
+            timeA={{ slug: 'bra'}}
+            timeB={{ slug: 'arg'}}
+            match={{ time: '7:00' }}
+          />
+          <Card
+            timeA={{ slug: 'equ'}}
+            timeB={{ slug: 'cat'}}
+            match={{ time: '7:00' }}
+          />
+        </section>
+      </main>
+    </>
+  )
+}
