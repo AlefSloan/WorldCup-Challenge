9d2d60c77c85a53c4eca1c6c0fa77fcc9d92adb7
Alef Sloan <alef_sloan@hotmail.com>
2022-10-10 21:01:05 -0300

    add Profile page

---
+++ WorldCup-Dats/src/pages/Profile/index.jsx
@@ -0,0 +1,51 @@
+import { Icon, Card } from '~/components'
+
+export const Profile = () => {
+  return (
+    <>
+      <header className='bg-red-300 text-white p-4'>
+        <div className='container max-w-3xl flex justify-between items-center p-4'>
+          <img src='./images/logo-red.svg' className='w-32'/>
+          <a href='/profile'>
+            <Icon name='profile' className='w-10' />
+          </a>
+        </div>
+      </header>
+      <main>
+        <section className='bg-red-300 text-white p-4'>
+          <div className='container max-w-3xl space-y-6 p-4'>
+            <a href='/dashboard' className='block'>
+              <Icon name='leftArrow' className='w-8'/>
+            </a>
+            <h2 className='text-white text-2xl font-bold'>Alef Sloan Rosado</h2>
+          </div>  
+        </section>
+        <div className='container max-w-3xl flex flex-col p-4 items-center'>
+          <h2 className='self-start text-red-300 font-bold text-2xl'>Seus Palpites</h2>
+          <div className='flex'>
+            <Icon name='leftArrow' className='h-8'/>
+            <h4 className='text-xl font-bold text-red-700'>20 de novembro</h4>
+            <Icon name='rightArrow' className='h-8'/>
+          </div>
+        </div>
+        <section id='content' className='container max-w-3xl p-4 space-y-6'>
+          <Card
+            timeA={{ slug: 'sui'}}
+            timeB={{ slug: 'cam'}}
+            match={{ time: '7:00' }}
+          />
+          <Card
+            timeA={{ slug: 'bra'}}
+            timeB={{ slug: 'arg'}}
+            match={{ time: '7:00' }}
+          />
+          <Card
+            timeA={{ slug: 'equ'}}
+            timeB={{ slug: 'cat'}}
+            match={{ time: '7:00' }}
+          />
+        </section>
+      </main>
+    </>
+  )
+}
