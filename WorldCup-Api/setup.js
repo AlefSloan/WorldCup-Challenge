69ba69d1f8b281e61dcad0732b5c63f4e719683d
Alef Sloan <alef_sloan@hotmail.com>
2022-10-11 17:56:49 -0300

    feat: add koa-bodyparser

--- WorldCup-Api/setup.js
+++ WorldCup-Api/setup.js
@@ -1,7 +1,10 @@
 import Koa from 'koa';
+import bodyParser from 'koa-bodyparser';
+
 import { router } from './router.js';
 
 export const app = new Koa();
 
+app.use(bodyParser());
 app.use(router.routes());
 app.use(router.allowedMethods());
