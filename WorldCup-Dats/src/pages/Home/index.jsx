f8d1e4a041f45cf547db71e73d8bd78c4d2410a5
Alef Sloan <alef_sloan@hotmail.com>
2022-10-10 21:01:48 -0300

    chore: update css on home page

--- WorldCup-Dats/src/pages/Home/index.jsx
+++ WorldCup-Dats/src/pages/Home/index.jsx
@@ -5,11 +5,11 @@ export function Home() {
         <img className='w-40' src='/images/logo-wine.svg' />
       </header>
       <main className="container max-w-5xl p-6 flex-1 p-4 flex items-center flex-col md:flex-row space-y-6 md:space-y-0 md:space-x-6">
-        <div className='md:flex-1 flex justify-center'>
+        <div className='md:flex-1 flex justify-center w-80 md:w-full'>
           <img className='w-full' src='/images/login-img.png'/>
         </div>
-        <div className='md:flex-1 flex flex-col space-y-6'>
-          <h1 className='text-3xl text-center md:text-left font-bold'>
+        <div className='md:flex-1 flex flex-col space-y-6 bg-red-700'>
+          <h1 className='text-xl text-center md:text-left md:text-3xl font-bold'>
             Dê o seu palpite na Copa do Mundo do Catar 2022!
           </h1>
           <a href='/signup' className='text-center bg-white text-red-700 text-xl px-8 py-5 rounded-xl'>
